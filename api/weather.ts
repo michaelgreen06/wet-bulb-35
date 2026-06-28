6653d97634cb6b2a670da0072c7126a88fe750a0
michaelgreen06 <michaelgreen06@gmail.com>
2026-06-27 20:45:57 -0600

    Fix Vercel static production deploy

--- api/weather.ts
+++
@@ -1,4 +0,0 @@
-import { fetchWeatherData } from "../lib/utils/weather";
-import { createWeatherHandler } from "../lib/server/weather-api.js";
-
-export default createWeatherHandler({ fetchWeatherData });
